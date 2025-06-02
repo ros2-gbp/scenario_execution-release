d959b0ce84f195bdb1eb0d0d69da792acd7725f5
Frederik Pasch <fred-labs@mailbox.org>
2025-06-02 18:10:46 +0200

    Revert "Trimmed the branch to only the scenario_execution_ros sub directory"
    This reverts commit 00f6df17b3535ef837d3d2ecc4aaec44b940b60e.

---
+++ scenario_execution/test/test_osc2_standard_osc.py
@@ -0,0 +1,48 @@
+# Copyright (C) 2024 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Test standard.osc parsing
+"""
+import unittest
+import py_trees
+from scenario_execution.model.osc2_parser import OpenScenario2Parser
+from scenario_execution.utils.logging import Logger
+from antlr4.InputStream import InputStream
+
+
+class TestOSC2Parser(unittest.TestCase):
+    # pylint: disable=missing-function-docstring, protected-access, no-member, unused-variable
+
+    def setUp(self) -> None:
+        self.parser = OpenScenario2Parser(Logger('test', False))
+        self.tree = py_trees.composites.Sequence(name="", memory=True)
+
+    def parse(self, scenario_content):
+        parsed_tree = self.parser.parse_input_stream(InputStream(scenario_content))
+        return self.parser.create_internal_model(parsed_tree, self.tree, "test.osc", False)
+
+    def test_standard_osc(self):
+        scenario_content = """
+import osc.standard
+"""
+        model = self.parse(scenario_content)
+
+    def test_standard_common_osc(self):
+        scenario_content = """
+import osc.standard.base
+"""
+        model = self.parse(scenario_content)
